e72659a95c7458b7a6266e3b21c2fcc518921bc5
tm30user <ademola@tm30.net>
2018-02-15 22:35:23 +0100

    fixing merge conflicts

---
+++ api.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tm30user
+ * Date: 05/02/2018
+ * Time: 6:34 PM
+ */
+return [
+    'url' => 'http://localhost:9060/v1/',
+    'cookie_domain' => 'localhost'
+];
